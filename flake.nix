8b04eb4d112492f8fe6cb59459320fbe3a491109
VipulOG <90324465+VipulOG@users.noreply.github.com>
2024-07-23 16:08:57 +0530

    Add pre-commit-hooks with nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    stylix.url = "github:danth/stylix";
 
     disko = {
       url = "github:nix-community/disko";
@@ -13,34 +15,52 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    stylix.url = "github:danth/stylix";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-  let
-    lib = nixpkgs.lib;
-    mylib = import ./lib { inherit lib inputs; };
-    commonArgs = system: { inherit lib mylib system; } // inputs;
+  outputs = inputs@{ self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      checks = forAllSystems (system: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      });
 
-  in {
-    nixosConfigurations = {
-      desktop = let system = "x86_64-linux"; in
-        mylib.nixosSystem {
-          modules = [
-            ./disko.nix { _module.args.device = "/dev/sdb"; }
-            inputs.stylix.nixosModules.stylix
-            ./modules/virtualization/docker.nix
-            ./modules/virtualization/podman.nix
-          ];
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        };
+      });
 
-          specialArgs = {
-            username = "vipul";
-            hostName = "desktop";
-            de = "gnome";
-            theme = "tomorrow-night";
-          } // commonArgs system;
+      nixosConfigurations =
+        let
+          lib = nixpkgs.lib;
+          mylib = import ./lib { inherit lib inputs; };
+          commonArgs = system: { inherit lib mylib system; } // inputs;
+        in
+        {
+          desktop = let system = "x86_64-linux"; in mylib.nixosSystem {
+            modules = [
+              ./disko.nix
+              { _module.args.device = "/dev/sdb"; }
+              inputs.stylix.nixosModules.stylix
+              ./modules/virtualization/docker.nix
+              ./modules/virtualization/podman.nix
+            ];
+            specialArgs = {
+              username = "vipul";
+              hostName = "desktop";
+              de = "gnome";
+              theme = "tomorrow-night";
+            } // commonArgs system;
+          };
         };
     };
-  };
 }
